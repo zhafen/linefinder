dbbf1c329826fb48fe9942ac4dc0223ad08baca0
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-06-05 15:44:28 -0500

    Updated the executable script for particle tracking.

--- particle_tracking/tracking_script.py
+++ particle_tracking/tracking_script.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 '''Script for tracking particles.
 
-@author: Daniel Angles-Alcazar, Zach Hafen
+@author: Zach Hafen
 @contact: zachary.h.hafen@gmail.com
 @status: Development
 '''
 
 import numpy as np
 
+import galaxy_finding
 import tracking
 
 ########################################################################
@@ -15,16 +16,27 @@ import tracking
 ########################################################################
 
 data_p = {
-  'sdir' : '/home1/03057/zhafen/repos/worldline/tests/test_data/test_data_with_new_id_scheme',
+  'sdir' : '../tests/test_data/test_data_with_new_id_scheme',
   'types' : [0,],
   'snap_ini' : 500,
   'snap_end' : 600,
-  'snap_step' : 10,
+  'snap_step' : 50,
 
   'target_ids' : np.array([ 36091289, 36091289, 3211791, 10952235 ]),
   'target_child_ids' : np.array([ 893109954, 1945060136, 0, 0 ]),
-  'outdir' : '/home1/03057/zhafen/repos/worldline/tests/test_data/tracking_output',
+  'outdir' : '../tests/test_data/tracking_output',
   'tag' : 'test',
+}
+
+########################################################################
+# Input Parameters for the Galaxy Finder
+########################################################################
+
+# Most (if not all) of the input parameters should be taken directly from the original ptrack data_p
+gal_finder_data_p = {
+  'sdir' : '../tests/test_data/ahf_test_data',
+  'tracking_dir' : data_p['outdir'],
+  'tag' : data_p['tag'],
 }
 
 ########################################################################
@@ -33,3 +45,10 @@ data_p = {
 
 particle_tracker = tracking.ParticleTracker( data_p )
 particle_tracker.save_particle_tracks()
+
+########################################################################
+# Run the Galaxy Finding
+########################################################################
+
+particle_track_gal_finder = galaxy_finding.ParticleTrackGalaxyFinder( gal_finder_data_p )
+particle_track_gal_finder.find_galaxies_for_particle_tracks()
