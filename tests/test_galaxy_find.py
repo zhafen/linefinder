3031ad6c2ecf7447e06505e67eadfb6748548184
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-06-26 15:30:37 -0500

    Further reduced the amount of test halo data I use.

--- tests/test_galaxy_find.py
+++ tests/test_galaxy_find.py
@@ -93,11 +93,11 @@ class TestGalaxyFinder( unittest.TestCase ):
     actual = self.galaxy_finder.find_mt_containing_halos( 2.5 )
 
     # Build the expected output
-    expected = np.zeros( (self.galaxy_finder.particle_positions.shape[0], 11) ).astype( bool )
+    expected = np.zeros( (self.galaxy_finder.particle_positions.shape[0], 6) ).astype( bool )
     expected[ 0, 0 ] = True
-    expected[ 0, 9 ] = True
+    expected[ 0, -2 ] = True
     expected[ 1, 0 ] = True
-    expected[ 1, 9 ] = True
+    expected[ 1, -2 ] = True
     expected[ 2, -1 ] = True
 
     npt.assert_allclose( actual, expected )
