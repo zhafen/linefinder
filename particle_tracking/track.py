754f1a9be6d8be1f52d6441d3bda323b96558fbe
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-07-04 15:09:44 -0500

    Removed the option to add AHF halo data during the tracking step (leftover from Daniel's pipeline.)

--- particle_tracking/track.py
+++ particle_tracking/track.py
@@ -22,11 +22,8 @@ import readsnap
 class ParticleTracker( object ):
   '''Searches IDs across snapshots, then saves the results.'''
 
-  def __init__( self, host_halo=False, **kwargs ):
+  def __init__( self,  **kwargs ):
     '''Setup the ID Finder. Looks for data in the form of "out_dir/ids_tag.hdf5"
-
-    Args:
-      host_halo (bool): Whether or not to include halo data for tracked particles during the tracking process.
 
     Keyword Args:
       Input Data Parameters:
@@ -43,9 +40,6 @@ class ParticleTracker( object ):
 
     # Save the kwargs
     self.kwargs = kwargs
-
-    # Save the arguments that have default values
-    self.host_halo = host_halo
 
   ########################################################################
 
@@ -136,10 +130,6 @@ class ParticleTracker( object ):
       'v':np.zeros( self.ntrack, dtype=(myfloat,(nsnap,3)) ), 
     }
 
-    if self.host_halo:
-      ptrack['HaloID'] = np.zeros(self.ntrack,dtype=('int32',(nsnap,)))
-      ptrack['SubHaloID'] = np.zeros(self.ntrack,dtype=('int32',(nsnap,)))
-
     ptrack['id'] = self.target_ids
     if self.target_child_ids is not None:
       ptrack['child_id'] = self.target_child_ids
@@ -152,7 +142,7 @@ class ParticleTracker( object ):
 
       id_finder = IDFinder()
       dfid, redshift, self.attrs = id_finder.find_ids( self.kwargs['sdir'], snum, self.kwargs['types'], self.target_ids, \
-                                           target_child_ids=self.target_child_ids, host_halo=self.host_halo )
+                                           target_child_ids=self.target_child_ids, )
 
       ptrack['redshift'][j] = redshift
       ptrack['snum'][j] = snum
@@ -164,10 +154,6 @@ class ParticleTracker( object ):
       ptrack['m'][:,j] = dfid['m'].values                                                               # Msun (particle mass in solar masses)
       ptrack['p'][:,j,:] = np.array( [ dfid['x0'].values, dfid['x1'].values, dfid['x2'].values ] ).T    # kpc (physical)
       ptrack['v'][:,j,:] = np.array( [ dfid['v0'].values, dfid['v1'].values, dfid['v2'].values ] ).T    # km/s (peculiar - need to add H(a)*r contribution)
-
-      if self.host_halo:
-        ptrack['HaloID'][:,j] = dfid['HaloID'].values
-        ptrack['SubHaloID'][:,j] = dfid['SubHaloID'].values
 
       j += 1
 
@@ -220,7 +206,7 @@ class IDFinder( object ):
 
   ########################################################################
 
-  def find_ids( self, sdir, snum, types, target_ids, target_child_ids=None, host_halo=0 ):
+  def find_ids( self, sdir, snum, types, target_ids, target_child_ids=None, ):
     '''Find the information for particular IDs in a given snapshot, ordered by the ID list you pass.
 
     Args:
@@ -228,7 +214,6 @@ class IDFinder( object ):
       snum (int): The snapshot to find the IDs for.
       types (list of ints): Which particle types to target.
       target_ids (np.array): The particle IDs you want to find.
-      host_halo (bool): Whether or not to include the host halo in the returned data. (Calculated via AHF)
 
     Returns:
       dfid (pandas.DataFrame): Dataframe for the selected IDs, ordered by target_ids.
@@ -250,10 +235,6 @@ class IDFinder( object ):
 
     # Find target_ids 
     self.dfid = self.select_ids()
-
-    # Add galaxy and halo data
-    if host_halo:
-      self.add_environment_data()
 
     return self.dfid, self.redshift, self.attrs
 
@@ -394,90 +375,4 @@ class IDFinder( object ):
     dfid = df.loc[ target_selection ]
 
     return dfid
-
-  ########################################################################
-
-  def add_environment_data(self):
-    #TODO: Remove.
-
-    AHFile = glob( sdir + '/snap' + g.snap_ext(snum) + 'Rpep..z*.AHF_particles')[0]
-
-    print '\nreading... ', AHFile
-
-    data = read_ahf_particles(AHFile)
-
-    hf = pd.DataFrame( data=data, index=data['id'] )
-
-    # WARNING:  order in hfid may **NOT** be the same as order in theIDS if there are reated indexes in hf !!!  (might be version dependent...)
-    hfid = hf.ix[target_ids].copy()             
-
-    del data, hf
-
-   ### NOTE: use keep='first' ??
-
-    # this keeps ONLY the FIRST value corresponding to each repeated id (i.e. now there are NO repeated IDs in hfid_host)
-    hfid_host = hfid.drop_duplicates('id', take_last=False, inplace=False)   
-    # this maintains the same order as target_ids because now hfid has NO repeated indexes (even if hfid has a different order)
-    dfid = dfid.join( hfid_host['HaloID'] )      
-    if not np.array_equal( target_ids, dfid['id'].values):
-      print 'WARNING!  issue with IDs (1)  !!!'
-
-    # this keeps ALL values EXCEPT the FIRST one for each repeated id (i.e. there might be repeated IDs in hfid_subhost)
-    hfid_subhost = hfid[ hfid.duplicated('id', take_last=False) ].copy()  
-    # this keeps ONLY the FIRST value corresponding to each repeated id (i.e. now there are NO repeated IDs in hfid_subhost)
-    hfid_subhost.drop_duplicates('id', take_last=False, inplace=True)    
-    hfid_subhost.rename(columns={'HaloID':'SubHaloID'}, inplace=True)
-    dfid = dfid.join( hfid_subhost['SubHaloID'] )
-    if not np.array_equal( target_ids, dfid['id'].values):
-      print 'WARNING!  issue with IDs (2)  !!!'
-
-    dfid['HaloID'][ pd.isnull(dfid['HaloID']) ] = -1
-    dfid['SubHaloID'][ pd.isnull(dfid['SubHaloID']) ] = -1
-
-########################################################################
-
-def read_ahf_particles(filename):
-  # TODO: Remove
-  '''Read a *.AHF_particles file. See http://popia.ft.uam.es/AHF/files/AHF.pdf,
-  pg 171 for documentation for such a file.
-
-  Args:
-    filename (str): The full file path.
-  '''
-
-  time_start = time.time()
-
-  # Get the number of lines
-  f = open(filename, "r")
-  Nhalos = int( f.readline() )
-  f.close()
-
-  df = pd.read_csv(filename, delim_whitespace=True, names=['id','Ptype'], header=0)
-
-  Nlines = df['id'].size
-  Ndata = Nlines - Nhalos
-
-  data = { 'id':np.zeros(Ndata,dtype='int64'), 'Ptype':np.zeros(Ndata,dtype='int8'), 'HaloID':np.zeros(Ndata,dtype='int32') }
-
-
-  jbeg = 0
-
-  for i in xrange(0, Nhalos):
-
-    jend = jbeg + df['id'][ jbeg+i ]
-
-    data['id'][ jbeg : jend ] = df['id'][ jbeg+i+1 : jend+i+1 ].values
-    data['Ptype'][ jbeg : jend ] = df['Ptype'][ jbeg+i+1 : jend+i+1 ].values
-    data['HaloID'][ jbeg : jend ] = df['Ptype'][ jbeg+i ]
-
-    if i in [1,10,100,1000,10000,100000]:
-      print '       HaloID =', df['Ptype'][ jbeg+i ], '   i = ', i
-
-    jbeg = jend
-
-  time_end = time.time()    
-
-  print 'read_ahf_particles done in ... ', time_end - time_start, ' seconds'
-
-  return data
 
