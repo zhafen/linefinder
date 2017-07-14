b583e1866e55dc4d2db9c62ee8b6e4f2946af464
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-07-14 16:37:20 -0500

    Classified Data can now be masked. Adapted the function to getting slices, given an integer.

--- tests/test_analyze_worldline_data/test_classified.py
+++ tests/test_analyze_worldline_data/test_classified.py
@@ -43,22 +43,6 @@ class TestClassifiedData( unittest.TestCase ):
 
   ########################################################################
 
-  def test_calc_base_fractions( self ):
-
-    actual = self.classified_data.calc_base_fractions()
-
-    expected = {
-      'fresh accretion' : 0.25,
-      'merger' : 0.5,
-      'intergalactic transfer' : 0.25,
-      'wind' : 0.5,
-    }
-
-    for key in actual.keys():
-      npt.assert_allclose( expected[key], actual[key] )
-
-  ########################################################################
-
   def test_get_data( self ):
 
     actual = self.classified_data.get_data( 'is_pristine' )
@@ -77,3 +61,57 @@ class TestClassifiedData( unittest.TestCase ):
     expected = np.array( [ 1, 0, ] ).astype( bool )
 
     npt.assert_allclose( expected, actual )
+
+  ########################################################################
+
+  def test_get_data_slice( self ):
+    
+    mask = np.array( [ 1, 0, 0, 1, ] ).astype( bool )
+    actual = self.classified_data.get_data( 'is_wind', mask=mask, slice_index=1 )
+
+    expected = np.array( [ 1, 1, ] ).astype( bool )
+
+    npt.assert_allclose( expected, actual )
+
+########################################################################
+
+class TestCalcBaseFractions( unittest.TestCase ):
+
+  def setUp( self ):
+
+    self.classified_data = classified.ClassifiedData( tracking_dir, tag )
+
+  ########################################################################
+
+  def test_calc_base_fractions( self ):
+
+    actual = self.classified_data.calc_base_fractions()
+
+    expected = {
+      'fresh accretion' : 0.25,
+      'merger' : 0.5,
+      'intergalactic transfer' : 0.25,
+      'wind' : 0.5,
+    }
+
+    for key in actual.keys():
+      npt.assert_allclose( expected[key], actual[key] )
+
+  ########################################################################
+
+  def test_calc_base_fractions_mask( self ):
+
+    mask = np.array( [ 1, 0, 1, 0, ] ).astype( bool )
+
+    actual = self.classified_data.calc_base_fractions( mask=mask )
+
+    expected = {
+      'fresh accretion' : 0.,
+      'merger' : 0.5,
+      'intergalactic transfer' : 0.5,
+      'wind' : 0.5,
+    }
+
+    for key in actual.keys():
+      npt.assert_allclose( expected[key], actual[key] )
+
--- worldline/analyze_worldline_data/classified.py
+++ worldline/analyze_worldline_data/classified.py
@@ -50,11 +50,36 @@ class ClassifiedData( object ):
 
   ########################################################################
 
-  def calc_base_fractions( self, return_or_store='return' ):
+  def get_data( self, data_key, mask=None, slice_index=None ):
+    '''Get the data from the data dictionary. Useful (over just accessing the array) when applying additional functions onto it.
+
+    Args:
+      data_key (str) : Key for the relevant data.
+      mask (np.array of bools) : What mask to apply to the data, if any
+      slice_index (int) : If getting only a particular slice (for the two dimensional arrays like 'is_wind), what slice?
+
+    Returns:
+      data_arr (np.array) : Array of the requested data
+    '''
+
+    data_arr = self.data[data_key]
+
+    if slice_index is not None:
+      data_arr = data_arr[:,slice_index]
+
+    if mask is not None:
+      data_arr = np.ma.masked_array( data_arr, mask=mask ).compressed()
+
+    return data_arr
+
+  ########################################################################
+
+  def calc_base_fractions( self, return_or_store='return', mask=None ):
     '''Get first order results of the classified data, in the form of fractions of each main type of classified data, relative to the total.
 
     Args:
       return_or_store (str) : Whether to return base_fractions or store it as an attribute
+      mask (np.array of bools) : What mask to apply to the data, if any
 
     Returns:
       base_fractions (dict) : Contains the fractions for each main type of classified data, evaluated at the final snapshot
@@ -63,36 +88,18 @@ class ClassifiedData( object ):
     base_fractions = {}
 
     # Get the total number of particles
-    n_classified = float( self.data['is_pristine'].size )
+    is_pristine = self.get_data( 'is_pristine', mask=mask )
+    n_classified = float( is_pristine.size )
 
-    base_fractions['fresh accretion'] = float( self.data['is_pristine'].sum() )/n_classified
-    base_fractions['merger'] = float( self.data['is_merger'].sum() )/n_classified
-    base_fractions['intergalactic transfer'] = float( self.data['is_mass_transfer'].sum() )/n_classified
+    base_fractions['fresh accretion'] = float( is_pristine.sum() )/n_classified
+    base_fractions['merger'] = float( self.get_data( 'is_merger', mask=mask ).sum() )/n_classified
+    base_fractions['intergalactic transfer'] = float( self.get_data( 'is_mass_transfer', mask=mask ).sum() )/n_classified
 
     # For wind, only evaluate at the last snapshot
-    base_fractions['wind'] = float( self.data['is_wind'][:,0].sum() )/n_classified
+    base_fractions['wind'] = float( self.get_data( 'is_wind', mask=mask, slice_index=0 ).sum() )/n_classified
 
     if return_or_store == 'return':
       return base_fractions
     else:
       self.base_fractions = base_fractions
 
-  ########################################################################
-
-  def get_data( self, data_key, mask=None ):
-    '''Get the data from the data dictionary. Useful (over just accessing the array) when applying additional functions onto it.
-
-    Args:
-      data_key (str) : Key for the relevant data.
-      mask (np.array of bools) : What mask to apply to the data, if any
-
-    Returns:
-      data_arr (np.array) : Array of the requested data
-    '''
-
-    data_arr = self.data[data_key]
-
-    if mask is not None:
-      data_arr = np.ma.masked_array( data_arr, mask=mask ).compressed()
-
-    return data_arr
