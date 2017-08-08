ce3947c84870e8526f3998eb92e72505085f2085
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-08-08 01:43:10 -0500

    Finished revising WorldlineSet. So much easier to use now, and will make analysis much easier for the forseeable future.

--- worldline/analyze_data/analyze_worldline_set.py
+++ worldline/analyze_data/analyze_worldline_set.py
@@ -6,7 +6,6 @@
 @status: Development
 '''
 
-import collections
 import numpy as np
 
 import analyze_worldlines
@@ -17,7 +16,8 @@ import galaxy_diver.utils.utilities as utilities
 ########################################################################
 
 class WorldlineSet( utilities.SmartDict ):
-  '''Container for multiple Worldlines classes.
+  '''Container for multiple Worldlines classes. The nice thing about this class is you can use it like a
+  Worldlines class, with the output being a dictionary of the different sets instead.
   '''
 
   def __init__( self, defaults, variations ):
@@ -38,83 +38,3 @@ class WorldlineSet( utilities.SmartDict ):
       worldlines_d[key] = analyze_worldlines.Worldlines( **kwargs )
 
     super( WorldlineSet, self ).__init__( worldlines_d )
-
-##class WorldlineSet( collections.Mapping ):
-##  '''Container for multiple Worldlines classes.
-##  '''
-##
-##  def __init__( self, defaults, variations ):
-##    '''
-##    Args:
-##      defaults (dict) : Set of default arguments for loading worldline data.
-##      variations (dict of dicts) : Labels and differences in arguments to be passed to Worldlines
-##    '''
-##
-##    # Load the worldline sets
-##    self._worldlines = {}
-##    for key in variations.keys():
-##      
-##      kwargs = dict( defaults )
-##      for var_key in variations[key].keys():
-##        kwargs[var_key] = variations[key][var_key]
-##
-##      self._worldlines[key] = analyze_worldlines.Worldlines( **kwargs )
-##
-##    self._worldlines = utilities.SmartDict( self._worldlines )
-##
-##  ########################################################################
-##  # Methods for Being Dictionary-Like
-##  ########################################################################
-##
-##  def __getitem__( self, key ):
-##    
-##    return self._worldlines[key]
-##
-##  ########################################################################
-##
-##  def __iter__( self ):
-##    return iter( self._worldlines )
-##
-##  ########################################################################
-##
-##  def __len__( self ):
-##    return len( self._worldlines )
-##
-##  ########################################################################
-##  # Methods for Getting Data from Worldlines
-##  ########################################################################
-##
-##  def __getattr__( self, attr, ):
-##    '''Replacement for default attribute retrieval. E.g. worldline_set.foo == worldline_set.__getattr__( 'foo' )
-##    Instead returns a dictionary containing the results of the attr for each Worldlines in the set.
-##    '''
-##
-##    results = {}
-##    for key in self.keys():
-##
-##      results[key] = utilities.deepgetattr( self._worldlines[key], attr )
-##
-##    return results
-##
-##  ########################################################################
-##
-##  def get( self, method, *args, **kwargs ):
-##    '''Generic getter method for any method in analyze_worldlines.Worldlines.
-##    This applies the chosen method across the full set of Worldlines.
-##
-##    Args:
-##      method (str) : The method of Worldlines you want to get out.
-##      *args, **kwargs : Arguments to supply to method.
-##
-##    Returns:
-##      results (dict) : results[key] is equal to WorldlineSet[key].method( *args, **kwargs ).
-##    '''
-##
-##    results = {}
-##    for key in self.keys():
-##
-##      key_method = utilities.deepgetattr( self._worldlines[key], method )
-##
-##      results[key] = key_method( *args, **kwargs )
-##
-##    return results
