f8801826d7369c310a7ba1f5126c47c0aa216c56
Zachary Hafen <zhafen@u.northwestern.edu>
2017-08-14 14:28:33 -0500

    Added a data container for the IDs that are being tracked.

---
+++ worldline/analyze_data/analyze_ids.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+'''Tools for reading worldline data
+
+@author: Zach Hafen
+@contact: zachary.h.hafen@gmail.com
+@status: Development
+'''
+
+import h5py
+import numpy as np
+import os
+
+########################################################################
+########################################################################
+
+class IDs( object ):
+  '''Loads and analyzes data created by select.py
+  '''
+
+  def __init__( self, data_dir, tag ):
+    '''
+    Args:
+      data_dir (str) : Data directory for the classified data
+      tag (str) : Identifying tag for the data to load.
+    '''
+
+    # Store the arguments
+    for arg in locals().keys():
+      setattr( self, arg, locals()[arg] )
+
+    # Open the file
+    ids_filepath = os.path.join( data_dir, 'ids_{}.hdf5'.format( tag ) )
+    with h5py.File( ids_filepath, 'r' ) as f:
+
+      # Store the data
+      self.data = {}
+      for key in f.keys():
+        if key != 'parameters':
+          self.data[key] = f[key][...]
+
+      # Store the data attributes
+      self.data_attrs = {}
+      for key in f.attrs.keys():
+        self.data_attrs[key] = f.attrs[key]
+
+      # Store the parameters
+      self.parameters = {}
+      param_grp = f['parameters']
+      for key in param_grp.attrs.keys():
+        self.parameters[key] = param_grp.attrs[key]
+
