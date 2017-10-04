42e52042522d50d3603192582f13536214c6fd65
zhafen_stampede <zachary.h.hafen@gmail.com>
2017-10-04 15:19:21 -0500

    Super minor update to analysis routines. Currently most changes to the particle tracking are happening in the backend, so they won't be as visible to anyone tracking this pipeline.

--- pathfinder/analyze_data/analyze_worldlines.py
+++ pathfinder/analyze_data/analyze_worldlines.py
@@ -476,7 +476,7 @@ class Worldlines( generic_data.GenericData ):
       galaxy_quantity = data_ma.sum( axis=0 )
 
       # Replace masked values with 0
-      if not isinstance( galaxy_quantity, float ):
+      if not isinstance( galaxy_quantity, np.float32 ):
         galaxy_quantity.fill_value = 0.
         galaxy_quantity = galaxy_quantity.filled()
 
