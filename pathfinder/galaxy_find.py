a43455c9be0e08ccc3b02e5b92aa3d0629cebb1b
Zach Hafen <zachary.h.hafen@gmail.com>
2018-01-04 18:25:47 -0600

    Debugging memory issues.......

--- pathfinder/galaxy_find.py
+++ pathfinder/galaxy_find.py
@@ -12,6 +12,7 @@ import numpy as np
 import os
 import sys
 import time
+from memory_profiler import profile
 
 import galaxy_diver.galaxy_finder.finder as galaxy_finder
 import galaxy_diver.read_data.ahf as read_ahf
@@ -32,7 +33,8 @@ class ParticleTrackGalaxyFinder( object ):
     '''Find the association with galaxies for entire particle tracks.'''
 
     @utilities.store_parameters
-    def __init__( self,
+    def __init__(
+        self,
         out_dir,
         tag,
         main_mt_halo_id,
@@ -42,11 +44,12 @@ class ParticleTrackGalaxyFinder( object ):
         ptracks_tag = default,
         galaxy_cut = d_constants.GALAXY_CUT,
         length_scale = d_constants.LENGTH_SCALE,
-        ids_to_return = [ 'gal_id', 'mt_gal_id', 'd_other_gal', 'd_other_gal_scaled', ],
+        ids_to_return = [
+            'gal_id', 'mt_gal_id', 'd_other_gal', 'd_other_gal_scaled', ],
         minimum_criteria = 'n_star',
         minimum_value = 10,
         n_processors = 1,
-        ):
+    ):
         '''Initialize.
 
         Args:
@@ -57,43 +60,51 @@ class ParticleTrackGalaxyFinder( object ):
                 Identifying tag.
 
             main_mt_halo_id (int) :
-                What is the ID of the main halo. To automatically choose via the most massive
-                at z=0, set equal to None. HOWEVER, be warned that the most massive halo at z=0 *may not* be the main halo.
+                What is the ID of the main halo. To automatically choose via the
+                most massive at z=0, set equal to None. HOWEVER, be warned that
+                the most massive halo at z=0 *may not* be the main halo.
 
             mtree_halos_index (str or int) :
                 The index argument to pass to AHFReader.get_mtree_halos().
-                For most cases this should be the final snapshot number, but see AHFReader.get_mtree_halos's documentation.
+                For most cases this should be the final snapshot number, but see
+                AHFReader.get_mtree_halos's documentation.
 
             halo_file_tag (str) :
-                What halo files to load (e.g. defaults to loading `halo_00000_smooth.dat`, etc.)?
+                What halo files to load (e.g. defaults to loading
+                `halo_00000_smooth.dat`, etc.)?
 
             ahf_data_dir (str, optional) :
-                Directory the AHF data is in. Defaults to the directory the simulation data is
-                stored in, as found in the ptracks file.
+                Directory the AHF data is in. Defaults to the directory the
+                simulation data is stored in, as found in the ptracks file.
 
             ptracks_tag (str, optional) :
                 Identifying tag for the ptrack data. Defaults to tag.
 
             galaxy_cut (float, optional) :
-                Anything within galaxy_cut*length_scale is counted as being inside the galaxy
+                Anything within galaxy_cut*length_scale is counted as being
+                inside the galaxy
 
             length_scale (str, optional) :
-                Anything within galaxy_cut*length_scale is counted as being inside the galaxy.
+                Anything within galaxy_cut*length_scale is counted as being
+                inside the galaxy.
 
             ids_to_return (list of strs, optional):
                 The types of id you want to get out.
 
             minimum_criteria (str, optional) :
                 Options...
-                'n_star' -- halos must contain a minimum number of stars to count as containing a galaxy.
-                'M_star' -- halos must contain a minimum stellar mass to count as containing a galaxy.
+                'n_star' -- halos must contain a minimum number of stars to
+                    count as containing a galaxy.
+                'M_star' -- halos must contain a minimum stellar mass to count
+                    as containing a galaxy.
 
             minimum_value (int or float, optional) :
                 The minimum amount of something (specified in minimum criteria)
                 in order for a halo to count as hosting a galaxy.
 
             n_processors (int) :
-                The number of processors to use. If parallel, expect significant memory usage.
+                The number of processors to use. If parallel, expect significant
+                memory usage.
         '''
 
         pass
@@ -105,10 +116,11 @@ class ParticleTrackGalaxyFinder( object ):
 
         self.time_start = time.time()
 
-        print "########################################################################"
+        print "#" * 80
         print "Starting Adding Galaxy and Halo IDs!"
-        print "########################################################################"
-        print "Using AHF data from this directory:\n    {}".format( self.ahf_data_dir )
+        print "#" * 80
+        print "Using halo data from this directory:\n    {}".format(
+            self.ahf_data_dir )
         print "Data will be saved here:\n    {}".format( self.out_dir )
         sys.stdout.flush()
 
@@ -123,12 +135,13 @@ class ParticleTrackGalaxyFinder( object ):
 
         time_end = time.time()
 
-        print "########################################################################"
+        print "#" * 80
         print "Done Adding Galaxy and Halo IDs!"
-        print "########################################################################"
+        print "#" * 80
         print "The data was saved at:\n    {}".format( self.save_filepath )
-        print "Took {:.3g} seconds, or {:.3g} seconds per particle!".format( time_end - self.time_start,
-                                                                                                                                            (time_end - self.time_start) / self.n_particles )
+        print "Took {:.3g} seconds, or {:.3g} seconds per particle!".format(
+            time_end - self.time_start,
+            (time_end - self.time_start) / self.n_particles )
 
     ########################################################################
 
@@ -139,6 +152,9 @@ class ParticleTrackGalaxyFinder( object ):
             self.ptrack (h5py file) : Loaded tracked particle data.
             self.ahf_reader (AHFReader instance): For the ahf data.
         '''
+
+        # DEBUG
+        print "Reading data."
 
         # Get the tag for particle tracking.
         if self.ptracks_tag is default:
@@ -173,33 +189,37 @@ class ParticleTrackGalaxyFinder( object ):
 
             # Get the data parameters to pass to GalaxyFinder
             kwargs = {
-                'ahf_reader' : self.ahf_reader,
-                'galaxy_cut' : self.galaxy_cut,
-                'length_scale' : self.length_scale,
-                'ids_to_return' : self.ids_to_return,
-                'minimum_criteria' : self.minimum_criteria,
-                'minimum_value' : self.minimum_value,
+                'ahf_reader': self.ahf_reader,
+                'galaxy_cut': self.galaxy_cut,
+                'length_scale': self.length_scale,
+                'ids_to_return': self.ids_to_return,
+                'minimum_criteria': self.minimum_criteria,
+                'minimum_value': self.minimum_value,
 
-                'redshift' : self.ptrack['redshift'][...][ i ],
-                'snum' : self.ptrack['snum'][...][ i ],
-                'hubble' : self.ptrack.attrs['hubble'],
-                'ahf_data_dir' : self.ahf_data_dir,
-                'mtree_halos_index' : self.mtree_halos_index,
-                'main_mt_halo_id' : self.main_mt_halo_id,
-                'halo_file_tag' : self.halo_file_tag,
+                'redshift': self.ptrack['redshift'][...][ i ],
+                'snum': self.ptrack['snum'][...][ i ],
+                'hubble': self.ptrack.attrs['hubble'],
+                'ahf_data_dir': self.ahf_data_dir,
+                'mtree_halos_index': self.mtree_halos_index,
+                'main_mt_halo_id': self.main_mt_halo_id,
+                'halo_file_tag': self.halo_file_tag,
             }
 
             time_start = time.time()
 
             # Find the galaxy for a given snapshot
-            gal_finder = galaxy_finder.GalaxyFinder( particle_positions, **kwargs )
+            gal_finder = galaxy_finder.GalaxyFinder(
+                particle_positions, **kwargs )
             galaxy_and_halo_ids = gal_finder.find_ids()
 
             time_end = time.time()
 
-            print 'Snapshot {:>3} | redshift {:>7.3g} | done in {:.3g} seconds'.format( kwargs['snum'],
-                                                                                                                                                                    kwargs['redshift'],
-                                                                                                                                                                    time_end - time_start )
+            print 'Snapshot {:>3} | redshift {:>7.3g} | done in {:.3g} seconds'\
+                .format(
+                    kwargs['snum'],
+                    kwargs['redshift'],
+                    time_end - time_start
+                )
             sys.stdout.flush()
 
             # Make the arrays to store the data in
@@ -207,13 +227,14 @@ class ParticleTrackGalaxyFinder( object ):
                 self.ptrack_gal_ids = {}
                 for key in galaxy_and_halo_ids.keys():
                     dtype = type( galaxy_and_halo_ids[key][0] )
-                    self.ptrack_gal_ids[key] = np.empty( ( gal_finder.n_particles, n_snaps ), dtype=dtype )
+                    self.ptrack_gal_ids[key] = np.empty(
+                        ( gal_finder.n_particles, n_snaps ), dtype=dtype )
 
             # Store the data in the primary array
             for key in galaxy_and_halo_ids.keys():
                 self.ptrack_gal_ids[key][ :, i ] = galaxy_and_halo_ids[key]
 
-            # Try clearing up memory again, in case gal_finder is hanging around...
+            # Try clearing up memory again, in case gal_finder is hanging around
             del kwargs
             del gal_finder
             del galaxy_and_halo_ids
@@ -228,6 +249,10 @@ class ParticleTrackGalaxyFinder( object ):
             self.ptrack_gal_ids (dict) : Where the galaxy IDs are stored.
         '''
 
+        # DEBUG
+        print "About to loop in parallel."
+
+        @profile
         def get_galaxy_and_halo_ids( args ):
             '''Get the galaxy and halo ids for a single snapshot.'''
 
@@ -235,15 +260,22 @@ class ParticleTrackGalaxyFinder( object ):
 
             time_start = time.time()
 
+            # DEBUG
+            print "Starting galaxy finding for snapshot {}".format( kwargs['snum'] )
+
             # Find the galaxy for a given snapshot
-            gal_finder = galaxy_finder.GalaxyFinder( particle_positions, **kwargs )
+            gal_finder = galaxy_finder.GalaxyFinder(
+                particle_positions, **kwargs )
             galaxy_and_halo_ids = gal_finder.find_ids()
 
             time_end = time.time()
 
-            print 'Snapshot {:>3} | redshift {:>7.3g} | done in {:.3g} seconds'.format( kwargs['snum'],
-                                                                                                                                                                    kwargs['redshift'],
-                                                                                                                                                                    time_end - time_start )
+            print 'Snapshot {:>3} | redshift {:>7.3g} | done in {:.3g} seconds'\
+                .format(
+                    kwargs['snum'],
+                    kwargs['redshift'],
+                    time_end - time_start
+                )
             sys.stdout.flush()
 
             # Try to avoid memory leaks
@@ -265,26 +297,27 @@ class ParticleTrackGalaxyFinder( object ):
 
             # Get the data parameters to pass to GalaxyFinder
             kwargs = {
-                'ahf_reader' : None,
-                'galaxy_cut' : self.galaxy_cut,
-                'length_scale' : self.length_scale,
-                'ids_to_return' : self.ids_to_return,
-                'minimum_criteria' : self.minimum_criteria,
-                'minimum_value' : self.minimum_value,
+                'ahf_reader': None,
+                'galaxy_cut': self.galaxy_cut,
+                'length_scale': self.length_scale,
+                'ids_to_return': self.ids_to_return,
+                'minimum_criteria': self.minimum_criteria,
+                'minimum_value': self.minimum_value,
 
-                'redshift' : self.ptrack['redshift'][...][ i ],
-                'snum' : self.ptrack['snum'][...][ i ],
-                'hubble' : self.ptrack.attrs['hubble'],
-                'ahf_data_dir' : self.ahf_data_dir,
-                'mtree_halos_index' : self.mtree_halos_index,
-                'main_mt_halo_id' : self.main_mt_halo_id,
-                'halo_file_tag' : self.halo_file_tag,
+                'redshift': self.ptrack['redshift'][...][ i ],
+                'snum': self.ptrack['snum'][...][ i ],
+                'hubble': self.ptrack.attrs['hubble'],
+                'ahf_data_dir': self.ahf_data_dir,
+                'mtree_halos_index': self.mtree_halos_index,
+                'main_mt_halo_id': self.main_mt_halo_id,
+                'halo_file_tag': self.halo_file_tag,
             }
 
             all_args.append( (particle_positions, kwargs) )
 
         # Actual parallel calculation
-        galaxy_and_halo_ids_all = mp_utils.parmap( get_galaxy_and_halo_ids, all_args, self.n_processors )
+        galaxy_and_halo_ids_all = mp_utils.parmap(
+            get_galaxy_and_halo_ids, all_args, self.n_processors )
 
         assert len( galaxy_and_halo_ids_all ) == n_snaps
 
@@ -296,13 +329,14 @@ class ParticleTrackGalaxyFinder( object ):
                 self.ptrack_gal_ids = {}
                 for key in galaxy_and_halo_ids.keys():
                     dtype = type( galaxy_and_halo_ids[key][0] )
-                    self.ptrack_gal_ids[key] = np.empty( ( n_particles, n_snaps ), dtype=dtype )
+                    self.ptrack_gal_ids[key] = np.empty(
+                        ( n_particles, n_snaps ), dtype=dtype )
 
             # Store the data in the primary array
             for key in galaxy_and_halo_ids.keys():
                 self.ptrack_gal_ids[key][ :, i ] = galaxy_and_halo_ids[key]
 
-            # Try clearing up memory again, in case gal_finder is hanging around...
+            # Try clearing up memory again, in case gal_finder is hanging around
             del galaxy_and_halo_ids
             gc.collect()
 
@@ -324,24 +358,27 @@ class ParticleTrackGalaxyFinder( object ):
         for key in self.ptrack_gal_ids.keys():
             f.create_dataset( key, data=self.ptrack_gal_ids[key] )
 
-        # Store the main mt halo id (as identified by the larges value at the lowest redshift)
+        # Store the main mt halo id
+        # (as identified by the larges value at the lowest redshift)
         if self.main_mt_halo_id is None:
             try:
                 indice = self.ahf_reader.mtree_halos[0].index.max()
             except AttributeError:
-                self.ahf_reader.get_mtree_halos( self.mtree_halos_index, self.halo_file_tag )
+                self.ahf_reader.get_mtree_halos(
+                    self.mtree_halos_index, self.halo_file_tag )
                 indice = self.ahf_reader.mtree_halos[0].index.max()
-            m_vir_z0 = self.ahf_reader.get_mtree_halo_quantity( quantity='Mvir', indice=indice,
-                                                                                                                    index=self.mtree_halos_index, tag=self.halo_file_tag )
+            m_vir_z0 = self.ahf_reader.get_mtree_halo_quantity(
+                quantity='Mvir', indice=indice,
+                index=self.mtree_halos_index, tag=self.halo_file_tag )
             f.attrs['main_mt_halo_id'] = m_vir_z0.argmax()
         else:
             f.attrs['main_mt_halo_id'] = self.main_mt_halo_id
 
-        grp = utilities.save_parameters( self, f )
+        utilities.save_parameters( self, f )
 
         # Save the current code version
         f.attrs['pathfinder_version'] = utilities.get_code_version( self )
-        f.attrs['galaxy_diver_version'] = utilities.get_code_version( read_ahf, instance_type='module' )
+        f.attrs['galaxy_diver_version'] = utilities.get_code_version(
+            read_ahf, instance_type='module' )
 
         f.close()
-
--- pathfinder/utils/data_constants.py
+++ pathfinder/utils/data_constants.py
@@ -23,24 +23,28 @@ PTYPE_STAR = 4
 ########################################################################
 # Global Default Parameters
 ########################################################################
-# I do it this way that way I can access these global defaults from plotting scripts as well,
+# I do it this way that way I can access these global defaults from plotting
+# scripts as well,
 # and also have all the most important parameters shown in one place.
 
 # The radius of the galaxy is defined as R_{gal} = GALAXY_CUT*LENGTH_SCALE
 LENGTH_SCALE = 'Rstar0.5'
-GALAXY_CUT = 2.0
+GALAXY_CUT = 5.0
 
-# Sometimes we may also require the following density cut for material to be part of a galaxy (n_b in cm^-3)
-GALAXY_DENSITY_CUT = 0.075 # I.e. nH = 0.1 cm^-3
+# Sometimes we may also require the following density cut for material to be
+# part of a galaxy (n_b in cm^-3)
+GALAXY_DENSITY_CUT = 0.075  # I.E. nH = 0.1 cm^-3
 
-# The ejection velocity necessary to be counted as a wind is defined as v_{wind} = WIND_CUT*VELOCITY_SCALE
-# There's also an absolute wind velocity in km/s required, v_{wind} = ABSOLUTE_WIND_CUT
-VELOCITY_SCALE = 'Vc(Rgal)'
+# The ejection velocity necessary to be counted as a wind is defined as
+# v_{wind} = WIND_CUT*VELOCITY_SCALE
+# There's also an absolute wind velocity in km/s required,
+# v_{wind} = ABSOLUTE_WIND_CUT
+VELOCITY_SCALE = 'Vmax'
 WIND_CUT = 1.0
 ABSOLUTE_WIND_CUT = 15.0
 
 # Fiducial threshold processing time, t_{pro}, in Myr.
 T_PRO = 100.0
 
-# Fiducial time interval during which material can be counted as merger (in Myr).
+# Fiducial time interval during which material can be counted as merger (in Myr)
 T_M = 500.0
