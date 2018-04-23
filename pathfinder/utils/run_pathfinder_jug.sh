f87911947518c6a39be4507dd584f5812ffe3893
Zach Hafen <zachary.h.hafen@gmail.com>
2018-04-23 00:06:39 -0500

    Added scripts for running pathfinder.

---
+++ pathfinder/utils/run_pathfinder_jug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=pathfinder
+#SBATCH --partition=skx-normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=48
+#SBATCH --time=3:00:00
+#SBATCH --output=/scratch/03057/zhafen/pathfinder_data/job_scripts/jobs/%j.out
+#SBATCH --error=/scratch/03057/zhafen/pathfinder_data/job_scripts/jobs/%j.err
+#SBATCH --mail-user=zhafen@u.northwestern.edu
+########################################################################
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+#SBATCH --account=TG-AST150059
+
+JUGFILE=/scratch/03057/zhafen/pathfinder_data/job_scripts/pathfinder_CGM_trove_jugfile.py
+
+for i in $(seq 10); do jug execute $JUGFILE & done
+
+while ! jug check $JUGFILE; do
+   jug sleep-until $JUGFILE 
+done
+
+
+echo 
+echo Done waiting!!!!
+echo 
