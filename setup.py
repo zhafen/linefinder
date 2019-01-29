6a05c3c7203eb7dd581ba67ceb6f331018ef8232
Zach Hafen <zachary.h.hafen@gmail.com>
2019-01-29 11:38:16 -0600

    Updated install files.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="linefinder",
-    version="0.8.1",
+    version="0.9",
     author="Zach Hafen",
     author_email="zachary.h.hafen@gmail.com",
     description="A tool for finding and classifying the worldlines of Lagrangian parcels of mass, in the context of hydrodynamic simulations of galaxy formation.",
@@ -15,19 +15,22 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'numpy>=1.14.5',
         'pandas>=0.20.3',
         'mock>=2.0.0',
+        'numpy>=1.15.4',
         'pytest>=3.4.0',
         'Jug>=1.6.7',
         'setuptools>=28.8.0',
+        'palettable>=3.1.1',
         'matplotlib>=2.0.2',
         'h5py>=2.7.0',
-        'galaxy_dive>=0.8.1.2',
-        'scipy>=1.1.0',
+        'GitPython>=2.1.11',
+        'scipy>=1.2.0',
+        'verdict>=1.0',
     ],
 )
