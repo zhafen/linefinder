4f3aa88d6b9ba6c04de2eade068c0278f4a18926
Zach Hafen <zachary.h.hafen@gmail.com>
2020-02-11 14:37:56 -0600

    Updated file management tool.

--- linefinder/utils/file_management.py
+++ linefinder/utils/file_management.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 '''Simple functions and variables for easily accessing common files and choices
 of parameters.
-
-@author: Zach Hafen
-@contact: zachary.h.hafen@gmail.com
-@status: Development
 '''
 
 import os
 
-import linefinder.config as config
+import stained_glass.config as sg_config
 
 ########################################################################
 ########################################################################
@@ -20,8 +16,10 @@ class FileManager( object ):
     def __init__( self, project=None ):
         self.project = project
 
-        parameters_name = '{}_PARAMETERS'.format( config.ACTIVE_SYSTEM.upper() )
-        self.system_parameters = getattr( config, parameters_name )
+        parameters_name = '{}_PARAMETERS'.format(
+            sg_config.ACTIVE_SYSTEM.upper()
+        )
+        self.system_parameters = getattr( sg_config, parameters_name )
 
         if project is not None:
             self.project_parameters = \
@@ -30,7 +28,13 @@ class FileManager( object ):
     ########################################################################
     ########################################################################
 
-    def get_sim_subdir( self, sim_name, physics=None, resolution=None, ):
+    def get_sim_subdir(
+        self,
+        sim_name,
+        physics = None,
+        resolution = None,
+        subsubdir = None,
+    ):
 
         if physics is None:
             name_mapping = {
@@ -39,52 +43,150 @@ class FileManager( object ):
             }
             physics = name_mapping[sim_name[4:]]
 
-        return os.path.join(
+        if resolution is None:
+            resolution = sg_config.DEFAULT_SIM_RESOLUTIONS[sim_name]
+
+        sim_subdir = os.path.join(
             physics,
             '{}_res{}'.format(
                 sim_name[:4],
-                config.DEFAULT_SIM_RESOLUTIONS[sim_name],
+                resolution,
             )
         )
 
+        if subsubdir is not None:
+            sim_subdir = os.path.join(
+                sim_subdir,
+                subsubdir,
+            )
+
+        return sim_subdir
+
     ########################################################################
 
-    def get_sim_dir( self, sim_name ):
+    def get_sim_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['simulation_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
             'output',
         )
 
     ########################################################################
 
-    def get_metafile_dir( self, sim_name ):
+    def get_yt_filepath( self, sim_name, snum, **kwargs ):
+
+        formatted_snum = '{:03d}'.format( snum )
+
+        # Assume there are subdirectories
+        filepath = os.path.join(
+            self.get_sim_dir( sim_name, **kwargs ),
+            'snapdir_{}'.format( formatted_snum ),
+            'snapshot_{}.0.hdf5'.format( formatted_snum )
+        )
+
+        # If there are not subdirectories change the filepath
+        if not os.path.isfile( filepath ):
+            filepath = os.path.join(
+                self.get_sim_dir( sim_name, **kwargs ),
+                'snapshot_{}.hdf5'.format( formatted_snum )
+            )
+
+        return filepath
+
+    ########################################################################
+
+    def get_sg_filepath(
+        self,
+        sim_name,
+        snum,
+        data_type = 'ray',
+        extension = 'h5',
+        i = 'first_available',
+        **kwargs
+    ):
+
+        full_data_dir = self.get_stained_glass_dir(
+            sim_name,
+            snum = snum,
+            **kwargs
+        )
+
+        # Get the filename, except for the ID
+        if i is None:
+            id_str = ''
+        else:
+            id_str = '_{:03d}'
+        empty_filename = '{}_snum{}{}.{}'.format(
+            data_type,
+            snum,
+            id_str,
+            extension
+        )
+        empty_filepath = os.path.join( full_data_dir, empty_filename )
+
+        # Get the file ID
+        if i == 'first_available':
+            i = 0
+            while True:
+                
+                filepath = empty_filepath.format( i )
+
+                if not os.path.isfile( filepath ):
+                    break
+                
+                i += 1
+        else:
+            filepath = empty_filepath.format( i )
+
+        return filepath
+
+    ########################################################################
+
+    def get_metafile_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['simulation_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
         )
 
     ########################################################################
 
-    def get_halo_dir( self, sim_name ):
+    def get_halo_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['halo_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
             'halo',
         )
 
     ########################################################################
 
-    def get_linefinder_dir( self, sim_name, subdir='data', ):
+    def get_stained_glass_dir(
+        self,
+        sim_name,
+        subdir = '',
+        snum = None,
+        makedirs = False,
+        **kwargs
+    ):
         
-        return os.path.join(
-            self.system_parameters['linefinder_data_dir'],
-            self.get_sim_subdir( sim_name ),
+        sg_dir = os.path.join(
+            self.system_parameters['stained_glass_data_dir'],
+            self.get_sim_subdir( sim_name, **kwargs ),
             subdir,
         )
+
+        if snum is not None:
+            sg_dir = os.path.join(
+                sg_dir,
+                'snum{:03d}'.format( snum ),
+            )
+
+        if makedirs:
+            os.makedirs( sg_dir, exist_ok=True )
+
+        return sg_dir
 
     ########################################################################
 
@@ -103,149 +205,4 @@ class FileManager( object ):
 
     ########################################################################
     ########################################################################
-
-    def get_linefinder_analysis_defaults(
-        self,
-        tag_tail,
-        sim_name = 'm12i',
-        ahf_index = 600,
-        ids_tag_tail = None,
-        ptracks_tag_tail = None,
-        galdef = '',
-    ):
-        '''Standard defaults for linefinder analysis routines.
-
-        Args:
-            tag_tail (str) :
-                The second half of the tag, after the simulation name.
-
-            sim_name (str) :
-                Name of the simulation to use.
-
-        Returns:
-            defaults (dict) :
-                Commonly used default dictionary.
-        '''
-
-        tag = '{}{}'.format( sim_name, tag_tail )
-
-        if galdef != '':
-            used_tag = '{}{}'.format( tag, galdef )
-        else:
-            used_tag = tag
-
-        defaults = {
-            'data_dir': self.get_linefinder_dir( sim_name ),
-            'tag': used_tag,
-
-            'halo_data_dir': self.get_halo_dir( sim_name ),
-            'ahf_index': ahf_index,
-            'main_halo_id': config.MAIN_MT_HALO_ID[sim_name],
-        }
-
-        if galdef != '':
-            defaults['ids_tag'] = tag
-            defaults['ptracks_tag'] = tag
-
-        if ids_tag_tail is not None:
-            defaults['ids_tag'] = '{}{}'.format( sim_name, ids_tag_tail )
-
-        if ptracks_tag_tail is not None:
-            defaults['ptracks_tag'] = '{}{}'.format(
-                sim_name,
-                ptracks_tag_tail,
-            )
-
-        return defaults
-
-    ########################################################################
-
-    def get_linefinder_analysis_variations(
-        self,
-        tag_tail,
-        default_sim_name = 'm12i',
-        sim_names = [ 'm12i', 'm12m', 'm12f', 'm12imd' ],
-        *args, **kwargs
-    ):
-        '''Standard default variations for linefinder analysis routines.
-
-        Args:
-            tag_tail (str) :
-                The second half of the tag, after the simulation name.
-
-            default_sim_name (str) :
-                Name of the simulation that's the "default".
-
-            sim_names (list of strs) :
-                What simulations to include.
-
-            *args, **kwargs :
-                Other arguments passed to
-                self.get_linefinder_analysis_defaults()
-
-        Returns:
-            variations (dict of dicts) :
-                Commonly used variations dictionary.
-        '''
-
-        variations = {}
-        for sim_name in sim_names:
-
-            if sim_name != default_sim_name:
-                variations[sim_name] = self.get_linefinder_analysis_defaults(
-                    tag_tail = tag_tail,
-                    sim_name = sim_name,
-                    *args, **kwargs
-                )
-            else:
-                variations[sim_name] = {}
-
-        return variations
-
-    ########################################################################
-
-    def get_linefinder_analysis_defaults_and_variations(
-        self,
-        tag_tail,
-        default_sim_name = 'm12i',
-        sim_names = [ 'm12i', 'm12m', 'm12f', ],
-        *args, **kwargs
-    ):
-        '''Standard defaults and variations for linefinder analysis routines.
-
-        Args:
-            tag_tail (str) :
-                The second half of the tag, after the simulation name.
-
-            default_sim_name (str) :
-                Name of the simulation that's the "default".
-
-            sim_names (list of strs) :
-                What simulations to include.
-
-            *args, **kwargs :
-                Other arguments passed to
-                self.get_linefinder_analysis_defaults() and
-                self.get_linefinder_analysis_variations() and
-
-        Returns:
-            variations (dict of dicts) :
-                Commonly used variations dictionary.
-        '''
-
-        linefinder_analysis_defaults = self.get_linefinder_analysis_defaults(
-            tag_tail,
-            sim_name = default_sim_name,
-            *args, **kwargs
-        )
-
-        linefinder_analysis_variations = \
-            self.get_linefinder_analysis_variations(
-                tag_tail,
-                default_sim_name = default_sim_name,
-                sim_names = sim_names,
-                *args, **kwargs
-            )
-
-        return linefinder_analysis_defaults, linefinder_analysis_variations
 
